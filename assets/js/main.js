61976cbe572a2236388c9cb8713bd49d7d6dfa7f
lucascholzeh <lucasschoff@gmail.com>
2025-08-06 12:37:25 -0300

    add: implement Pokemon list rendering and API fetching

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+                
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">Grass</li>
+                    <li class="type">Poison</li>
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>
+                
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList');
+
+// Chama a função getPokemons do objeto pokeApi para obter a lista de Pokémon
+pokeApi.getPokemons().then((pokemons = []) => { 
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+})
